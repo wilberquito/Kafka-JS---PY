d9c075fab7e140432e760a82253b2c519d361f1c
wilberquito <wbq.software@gmail.com>
2022-05-13 08:48:48 +0200

    feat: js consumer

---
+++ JSconsumer/main.js
@@ -0,0 +1,32 @@
+import { Kafka } from 'kafkajs'
+import { readFileSync } from 'fs'
+
+const data = readFileSync('settings.json')
+const settings = JSON.parse(data);
+
+const socket = settings.socket;
+const groupId = settings.groupId;
+const topic = settings.topic;
+
+const kafka = new Kafka({ brokers: [socket] })
+
+const consumer = kafka.consumer({ groupId: groupId })
+
+async function daemon() {
+    await consumer.connect()
+    await consumer.subscribe({ topic: topic, fromBeginning: true })
+
+    await consumer.run({
+        autoCommit: 1,
+        eachMessage: async ({ message }) => {
+            console.log({
+                date: new Date(),
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+
+daemon();
+
+
