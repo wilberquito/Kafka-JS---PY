419ad2820d3e6e2c9ffeaaceb4f38211de36049b
wilberquito <wbq.software@gmail.com>
2022-06-20 11:20:53 +0200

    write in file kafka commits and log them

--- JSconsumer/main.js
+++ JSconsumer/main.js
@@ -1,5 +1,5 @@
-import { Kafka } from 'kafkajs'
-import { readFileSync } from 'fs'
+import { appendFile, readFileSync } from 'fs';
+import { Kafka } from 'kafkajs';
 
 const data = readFileSync('../settings.json')
 const settings = JSON.parse(data);
@@ -16,15 +16,18 @@ const consumer = kafka.consumer({ groupId: groupId })
 
 async function daemon() {
     await consumer.connect()
-    await consumer.subscribe({ topic: topic })
+    await consumer.subscribe({ topic: topic, fromBeginning: false })
 
     await consumer.run({
         eachMessage: async ({ message }) => {
-            console.log({
+            const commit = {
                 date: new Date(),
                 message: message.value.toString(),
                 offset: message.offset
-            })
+            }
+            const str = JSON.stringify(commit)
+            appendFile('export.txt', str, () => console.log(commit))
+            // console.log(commit)
         },
     })
 }
